a6e315c075a5f1c4741d36295f9cbbf51124d6d8
Terrafirst <meghachavan1997@gmail.com>
2025-11-05 11:17:10 +0530

    Create student roll no and name

--- src/Homepage/demo.java
+++ src/Homepage/demo.java
@@ -1,5 +1,15 @@
 package Homepage;
 
 public class demo {
+	
+	public static void main(String[] args) {
+		
+		int rollNo=3;
+		String StudentName="Megha";
+		
+		System.out.println("rollNo of student is:" + rollNo);
+		
+		System.out.println("Student name  is:" + StudentName);
+	}
 
 }
